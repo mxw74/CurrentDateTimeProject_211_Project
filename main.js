37bca9dbf4bd390f350f1f3390c34a5b38701095
mxw74 <marcus.walls@utexas.edu>
2022-06-28 14:57:05 -0500

    do some basic js functions

--- main.js
+++ main.js
@@ -15,11 +15,36 @@ const displayDate = () => {
  
 
 // Write a JavaScript program to convert a number to a string.
+const numToString = () => {
 
+  let convertNum = 10;
+
+  let text = convertNum.toString()
+
+  document.getElementById("numToString").innerHTML = text
+
+  return text 
+
+}
 
 
 // Write a JavaScript program to convert a string to the number.
+const stringToNum =() => {
 
+    //var to hold a string
+    let newString = "28"
+
+    //another variable to hold the number converted from the string
+    let convString = parseInt(newString)
+
+    //console.log to check my work and see if it returns right
+    console.log(convString)
+    //return the number
+
+    document.getElementById("stringToNum").innerHTML = convString
+    return(convString)
+
+}
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -33,18 +58,56 @@ const displayDate = () => {
 
   
 // Write a JavaScript program that adds 2 numbers together.
+const addition = () => {
 
+    const num1 = (10) //define first number
+
+    const num2 = (10) //define second number
+
+    let sum = num1 + num2; //create the function
+
+    document.getElementById("addition").innerHTML = sum
+
+    return sum //return the results
+}
 
 
 // Write a JavaScript program that runs only when 2 things are true.
+const twoTrues = () => {
+  //make variable to be determined true or not
+  const firstNum = (10);
+
+  const secondNum = (5);
+
+  if (firstNum===10 && secondNum===5) {
+      document.getElementById("twoTrues").innerHTML = response = "Run Program";
+  } else {
+    document.getElementById("twoTrues").innerHTML = response = "Don't Run";
+  }
+}
+
+
+  //if 2+ are true then run program else dont run program
 
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+  //make variables to be checked for true or not
+
+  //make js count how many are true or not
+
+  //make it run if 1 of 2 items is true
+
 
 
 // Write a JavaScript program that runs when both things are not true.  
+
+  //make variables to be checked for true or not
+
+  //make js check how many true and how many false
+
+  //if there are no true then make it run
 
 // ***************************
 //         PART TWO
